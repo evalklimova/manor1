dc2fe45b944cb671345ccabf00c0268ecf956f4e
evalklimova <evelina1limova@gmail.com>
2024-11-05 23:01:13 +0300

    Add files via upload

--- js/file.js
+++ js/file.js
@@ -1,1 +1,26 @@
+function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+}
 
+function getCentury(year) {
+    return Math.ceil(year / 100);
+}
+
+function checkYear() {
+    const year = document.getElementById("yearInput").value;
+    const resultContainer = document.getElementById("resultContainer");
+    
+    if (year) {
+        const parsedYear = parseInt(year);
+        const leapYear = isLeapYear(parsedYear);
+        const century = getCentury(parsedYear);
+
+        let resultMessage = `${parsedYear} год `;
+        resultMessage += leapYear ? "является високосным. " : "не является високосным. ";
+        resultMessage += `Этот год относится к ${century} веку.`;
+
+        resultContainer.innerText = resultMessage;
+    } else {
+        resultContainer.innerText = "Пожалуйста, введите корректный год.";
+    }
+}
